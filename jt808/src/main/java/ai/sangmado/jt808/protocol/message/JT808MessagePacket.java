41ad2965265eace0d67f8a2ad3e08e9f31b7986a
Chundong Gao <gaochundong@momenta.ai>
2020-01-23 11:13:39 +0800

    从消息头首字节开始，同后一字节进行异或操作，直到消息体末字节结束。校验码占用一个字节。

--- jt808/src/main/java/ai/sangmado/jt808/protocol/message/JT808MessagePacket.java
+++ jt808/src/main/java/ai/sangmado/jt808/protocol/message/JT808MessagePacket.java
@@ -34,7 +34,7 @@ public class JT808MessagePacket implements IJT808MessageFormatter {
 
     /**
      * 校验码
-     * 从消息头开始，同后一字节异或，直到校验码前一个字节，占用一个字节。
+     * 从消息头首字节开始，同后一字节进行异或操作，直到消息体末字节结束。校验码占用一个字节。
      */
     private Byte checksum;
 
@@ -46,44 +46,6 @@ public class JT808MessagePacket implements IJT808MessageFormatter {
     @Override
     public void serialize(ISpecificationContext ctx, IJT808MessageBufferWriter writer) {
 
-        getHeader().getMessageContentProperty().setContentLength(1);
-//
-//        // ---------------开始组包--------------
-//        // 1.起始符
-//        writer.writeByte(getBeginMarker());
-//
-//
-//
-//        // 2.写入头部 //部分有带数据体的长度，那么先跳过写入头部部分
-//        //  2.1.消息ID
-//        writer.writeWord(getHeader().getMessageId().getValue());
-//        //  2.2.消息体属性(包含消息体长度所以先跳过)
-//        writer.Skip(2, out int msgBodiesPropertyPosition);
-//
-//
-//
-//        int headerLength = writer.GetCurrentPosition();
-//        // 3.处理数据体部分
-//        if (value.Bodies != null)
-//        {
-//            if (!value.Bodies.SkipSerialization)
-//            {
-//                JT808MessagePackFormatterResolverExtensions.JT808DynamicSerialize(value.Bodies,
-//                        ref writer, value.Bodies,
-//                        config);
-//            }
-//        }
-//        //  3.1.处理数据体长度
-//        // 2.2.回写消息体属性
-//        value.Header.MessageBodyProperty.DataLength = (writer.GetCurrentPosition() - headerLength);
-//        writer.WriteUInt16Return(value.Header.MessageBodyProperty.Wrap(), msgBodiesPropertyPosition);
-//        // 4.校验码
-//        writer.WriteXor();
-//        // 5.终止符
-//        writer.WriteEnd();
-//        // 6.编码
-//        writer.WriteEncode();
-//        // ---------------组包结束--------------
     }
 
     @Override
