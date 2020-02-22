41732472a124cfdc1c589e8dd351b885e5363774
Dennis Gao <gaochundong@hotmail.com>
2020-02-23 00:58:37 +0800

    2019版本中增加了报警信息的位置相关性

--- jt809/src/main/java/ai/sangmado/jt809/protocol/enums/JT809WarningType.java
+++ jt809/src/main/java/ai/sangmado/jt809/protocol/enums/JT809WarningType.java
@@ -14,36 +14,53 @@ import static ai.sangmado.jt809.protocol.enums.JT809ProtocolVersion.V2019;
  */
 @Getter
 public enum JT809WarningType {
-    JT809_Warning_0x0001(0x0001, V2011, "超速报警"),
-    JT809_Warning_0x0002(0x0002, V2011, "疲劳驾驶报警"),
-    JT809_Warning_0x0003(0x0003, V2011, "紧急报警"),
-    JT809_Warning_0x0004(0x0004, V2011, "进入指定区域报警"),
-    JT809_Warning_0x0005(0x0005, V2011, "离开指定区域报警"),
-    JT809_Warning_0x0006(0x0006, V2011, "路段赌赛报警"),
-    JT809_Warning_0x0007(0x0007, V2011, "危险路段报警"),
-    JT809_Warning_0x0008(0x0008, V2011, "越界报警"),
-    JT809_Warning_0x0009(0x0009, V2011, "盗警"),
-    JT809_Warning_0x000A(0x000A, V2011, "劫警"),
-    JT809_Warning_0x000B(0x000B, V2011, "偏离路线报警"),
-    JT809_Warning_0x000C(0x000C, V2011, "车辆移动报警"),
-    JT809_Warning_0x000D(0x000D, V2011, "超时驾驶报警"),
-    JT809_Warning_0x0010(0x0010, V2019, "违规行驶报警"),
-    JT809_Warning_0x0011(0x0011, V2019, "前撞报警"),
-    JT809_Warning_0x0012(0x0012, V2019, "车道偏离报警"),
-    JT809_Warning_0x0013(0x0013, V2019, "胎压异常报警"),
-    JT809_Warning_0x0014(0x0014, V2019, "动态信息异常报警"),
-    JT809_Warning_0x00FF(0x00FF, V2011, "其他报警"),
+    JT809_Warning_0x0001(0x0001, V2011, true, "超速报警"),
+    JT809_Warning_0x0002(0x0002, V2011, true, "疲劳驾驶报警"),
+    JT809_Warning_0x0003(0x0003, V2011, true, "紧急报警"),
+    JT809_Warning_0x0004(0x0004, V2011, true, "进入指定区域报警"),
+    JT809_Warning_0x0005(0x0005, V2011, true, "离开指定区域报警"),
+    JT809_Warning_0x0006(0x0006, V2011, true, "路段赌赛报警"),
+    JT809_Warning_0x0007(0x0007, V2011, true, "危险路段报警"),
+    JT809_Warning_0x0008(0x0008, V2011, true, "越界报警"),
+    JT809_Warning_0x0009(0x0009, V2011, true, "盗警"),
+    JT809_Warning_0x000A(0x000A, V2011, true, "劫警"),
+    JT809_Warning_0x000B(0x000B, V2011, true, "偏离路线报警"),
+    JT809_Warning_0x000C(0x000C, V2011, true, "车辆移动报警"),
+    JT809_Warning_0x000D(0x000D, V2011, true, "超时驾驶报警"),
+    JT809_Warning_0x0010(0x0010, V2019, true, "违规行驶报警"),
+    JT809_Warning_0x0011(0x0011, V2019, true, "前撞报警"),
+    JT809_Warning_0x0012(0x0012, V2019, true, "车道偏离报警"),
+    JT809_Warning_0x0013(0x0013, V2019, true, "胎压异常报警"),
+    JT809_Warning_0x0014(0x0014, V2019, true, "动态信息异常报警"),
+    JT809_Warning_0x00FF(0x00FF, V2011, true, "其他位置相关报警"),
+
+    JT809_Warning_0xA001(0xA001, V2019, false, "超时停车"),
+    JT809_Warning_0xA002(0xA002, V2019, false, "车辆定位信息上报时间间隔异常"),
+    JT809_Warning_0xA003(0xA003, V2019, false, "车辆定位信息上报距离间隔异常"),
+    JT809_Warning_0xA004(0xA004, V2019, false, "下级平台异常断线"),
+    JT809_Warning_0xA005(0xA005, V2019, false, "下级平台数据传输异常"),
+    JT809_Warning_0xA006(0xA006, V2019, false, "路段赌赛报警"),
+    JT809_Warning_0xA007(0xA007, V2019, false, "危险路段报警"),
+    JT809_Warning_0xA008(0xA008, V2019, false, "雨雪天气报警"),
+    JT809_Warning_0xA009(0xA009, V2019, false, "驾驶员身份异常识别"),
+    JT809_Warning_0xA00A(0xA00A, V2019, false, "终端异常(含线路连接异常)"),
+    JT809_Warning_0xA00B(0xA00B, V2019, false, "平台接入异常"),
+    JT809_Warning_0xA00C(0xA00C, V2019, false, "核心数据异常"),
+    JT809_Warning_0xA0FF(0xA0FF, V2019, false, "其他非位置相关报警"),
     ;
 
     private Long value;
 
     private IProtocolVersion since;
 
+    private Boolean locationBased;
+
     private String description;
 
-    JT809WarningType(long value, IProtocolVersion since, String description) {
+    JT809WarningType(long value, IProtocolVersion since, boolean locationBased, String description) {
         this.value = value;
         this.since = since;
+        this.locationBased = locationBased;
         this.description = description;
     }
 
